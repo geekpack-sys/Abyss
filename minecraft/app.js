be02613747809a8d0a01b41c7faa3a22fff8bd2a
geekpack-sys <subtomininooby@gmail.com>
2026-05-22 15:14:48 +0200

    help me

--- minecraft/app.js
+++ minecraft/app.js
@@ -2,7 +2,7 @@ const Players_online_text = document.getElementById("players")
 const Is_server_online = document.getElementById("status")
 
 async function GetData() {
-    const url = "https://api.mcsrvstat.us/3/belgianfries2.aternos.me:61388"
+    const url = "https://api.mcstatus.io/v2/status/java/belgianfries2.aternos.me:61388"
 
     try {
         const response = await fetch(url)
