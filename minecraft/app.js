b7e11fb714364fcc62e07e87c4db9d682c57bd93
geekpack-sys <subtomininooby@gmail.com>
2026-05-22 15:06:20 +0200

    Changed URL and shit

--- minecraft/app.js
+++ minecraft/app.js
@@ -2,11 +2,10 @@ const Players_online_text = document.getElementById("players")
 const Is_server_online = document.getElementById("status")
 
 async function GetData() {
-    const url = "https://api.mcsrvstat.us/3/"
-    const Server_Domein = "BelgianFries2.aternos.me"
+    const url = "https://api.mcsrvstat.us/3/BelgianFries2.aternos.me"
 
     try {
-        const response = await fetch(url + Server_Domein)
+        const response = await fetch(url)
 
         if (!response.ok) {
             throw new Error(`Response status: ${response.status}`);
@@ -27,7 +26,7 @@ Data = GetData()
 
 isOnline = Data.online;
 
-if (isOnline == true) {
+if (isOnline) {
     Is_server_online.innerHTML = "Online"
 } else {
     Is_server_online.innerHTML = "Offline"
