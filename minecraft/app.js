c34d7f16908654672099b3cdb3dd689c508e2465
geekpack-sys <subtomininooby@gmail.com>
2026-05-23 10:49:18 +0200

    Made a button to start the server

--- minecraft/app.js
+++ minecraft/app.js
@@ -1,5 +1,7 @@
 const Players_online_text = document.getElementById("players")
 const Is_server_online = document.getElementById("status")
+const Startbutton = document.getElementById("Start")
+
 
 async function GetData() {
     const url = "https://api.mcstatus.io/v2/status/java/mc.abyss.work.gd:61388"
@@ -31,3 +33,26 @@ if (isOnline) {
 } else {
     Is_server_online.innerHTML = "Offline"
 }
+
+
+function start_server() {
+    await fetch("https://aternos.org/ajax/server/start?access-credits=false&TOKEN=iGGf0dbSVtxCCBFTG6EF&SEC=uBqXlA1eJyMHGTw8%3ArgdXP87TScAb2cBq&SERVER=dmlaAN2GCtBquU3p", {
+    "credentials": "include",
+    "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "Priority": "u=0",
+        "Pragma": "no-cache",
+        "Cache-Control": "no-cache"
+    },
+    "referrer": "https://aternos.org/server/",
+    "method": "GET",
+    "mode": "cors"
+    });
+}
+
+Startbutton.addEventListener("click", start_server())
