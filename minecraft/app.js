f51cbf85d1f98f942e7008a8624f4c316b0b4793
geekpack-sys <subtomininooby@gmail.com>
2026-05-27 15:16:28 +0200

    idk

--- minecraft/app.js
+++ minecraft/app.js
@@ -39,17 +39,6 @@ async function start_server() {
     try {
         const response = await fetch("https://aternos.org/ajax/server/start?access-credits=false&TOKEN=iGGf0dbSVtxCCBFTG6EF&SEC=uBqXlA1eJyMHGTw8%3ArgdXP87TScAb2cBq&SERVER=dmlaAN2GCtBquU3p", {
             credentials: "include",
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:151.0) Gecko/20100101 Firefox/151.0",
-                "Accept": "*/*",
-                "Accept-Language": "en-US,en;q=0.9",
-                "Sec-Fetch-Dest": "empty",
-                "Sec-Fetch-Mode": "cors",
-                "Sec-Fetch-Site": "same-origin",
-                "Priority": "u=0",
-                "Pragma": "no-cache",
-                "Cache-Control": "no-cache"
-            },
             referrer: "https://aternos.org/server/",
             method: "GET",
             mode: "cors"
